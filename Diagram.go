8a9d6d0c5cc460328711a228d9d063b2c864d834
Sina <sina.majdieh@gmail.com>
2020-01-10 03:53:06 -0500

    A very simple chart

--- Diagram.go
+++ Diagram.go
@@ -11,27 +11,27 @@ const (
 )
 
 type chart struct {
-	Yaxis                [max_Yaxis]float64
-	Xaxis                [max_Xaxis]float64
-	Yaxis_end            float64
-	Xaxis_end            float64
+	yaxis                [max_Yaxis]float64
+	xaxis                [max_Xaxis]float64
+	yaxis_end            float64
+	xaxis_end            float64
 	diagram              [][]string
 	diagram_Yaxis_length int
 	diagram_Xaxis_length int
 }
 
 func (c *chart) make( /*accuracy float64 ,*/ max_Yaxis_number float64, max_Xaxis_number float64) {
-	c.Yaxis_end = max_Yaxis_number
-	c.Xaxis_end = max_Xaxis_number
+	c.yaxis_end = max_Yaxis_number
+	c.xaxis_end = max_Xaxis_number
 	c.diagram_Yaxis_length = 1
 	c.diagram_Xaxis_length = 1
-	for i := 1; i < max_Yaxis && c.Yaxis[i-1] != max_Yaxis_number; i++ {
+	for i := 1; i < max_Yaxis && c.yaxis[i-1] != max_Yaxis_number; i++ {
 		//fmt.Println(c.Yaxis[i-1])
-		c.Yaxis[i] = c.Yaxis[i-1] + accuracy
+		c.yaxis[i] = c.yaxis[i-1] + accuracy
 		c.diagram_Yaxis_length++
 	}
-	for i := 1; i < max_Xaxis && c.Xaxis[i-1] != max_Xaxis_number; i++ {
-		c.Xaxis[i] = c.Xaxis[i-1] + accuracy
+	for i := 1; i < max_Xaxis && c.xaxis[i-1] != max_Xaxis_number; i++ {
+		c.xaxis[i] = c.xaxis[i-1] + accuracy
 		c.diagram_Xaxis_length++
 	}
 	c.diagram = make([][]string, c.diagram_Yaxis_length)
@@ -45,13 +45,13 @@ func (c *chart) make( /*accuracy float64 ,*/ max_Yaxis_number float64, max_Xaxis
 
 func (c *chart) findpoint(y, x float64) (Y int, X int) {
 	for i := 0; i < c.diagram_Yaxis_length; i++ {
-		if c.Yaxis[i] == y {
+		if c.yaxis[i] == y {
 			Y = i
 			break
 		}
 	}
 	for i := 0; i < c.diagram_Xaxis_length; i++ {
-		if c.Xaxis[i] == x {
+		if c.xaxis[i] == x {
 			X = i
 			break
 		}
@@ -63,7 +63,7 @@ func (c *chart) findpoint(y, x float64) (Y int, X int) {
 //Adding a point to Diagram
 func (c *chart) AddValue(x float64, y float64) {
 	var Yindex, Xindex int
-	if y <= c.Yaxis_end && x <= c.Xaxis_end {
+	if y <= c.yaxis_end && x <= c.xaxis_end {
 		Yindex, Xindex = c.findpoint(y, x)
 	} else {
 		fmt.Println("not available")
